67b788984f2b1fdabc80e6a7497a3abc75ba18cf
Pranay Joshi <pranayj1449517@gmail.com>
2017-05-20 19:35:21 +0530

    done

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -211,24 +211,6 @@ angular.module('starter.controllers', ['angular-svg-round-progressbar', 'starter
   };
 })
 
-.controller('BrowseMoreCtrl', function ($scope, $stateParams, MyServices,$state) {
-
-  $scope.goBackHandler = function () {
-    window.history.back(); //This works
-  };
-  $scope.product= {}
-  // alert($stateParams.category);
-  $scope.product.category = $stateParams.category;
-
-  console.log("dsjh",$scope.product,$stateParams)
-  MyServices.products($scope.product,function(data) {
-
-            console.log(data);
-            $scope.prod = data.data;
-            console.log("proctid",$scope.prod);
-
-  });
-})
 
 
 .controller('AuthPaymentCtrl', function ($scope, $stateParams) {
@@ -238,38 +220,7 @@ angular.module('starter.controllers', ['angular-svg-round-progressbar', 'starter
 })
 
 
-.controller('BrowseCtrl', function ($scope, $ionicSlideBoxDelegate, MyServices,$state) {
-  $scope.closePopover = function () {
-      $scope.terms.hide();
-    };
 
-    var ipObj1 = {
-      callback: function (val) { //Mandatory
-        console.log('Return value from the datepicker popup is : ' + val, new Date(val));
-      },
-      disabledDates: [ //Optional
-        new Date(2016, 2, 16),
-        new Date(2015, 3, 16),
-        new Date(2015, 4, 16),
-        new Date(2015, 5, 16),
-        new Date('Wednesday, August 12, 2015'),
-        new Date("08-16-2016"),
-        new Date(1439676000000)
-      ],
-      from: new Date(2012, 1, 1), //Optional
-      to: new Date(2016, 10, 30), //Optional
-      inputDate: new Date(), //Optional
-      mondayFirst: true, //Optional
-      disableWeekdays: [0], //Optional
-      closeOnSelect: false, //Optional
-      templateType: 'popup' //Optional
-    };
-
-    $scope.openDatePicker = function () {
-      ionicDatePicker.openDatePicker(ipObj1);
-    };
-
-  })
 
 
   .controller('Subpage1Ctrl', function ($scope, $stateParams) {
