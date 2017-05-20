e064c8cdd013beee61569902ba8dfb52c029a615
Pranay Joshi <pranayj1449517@gmail.com>
2017-05-20 19:30:43 +0530

    done

--- www/js/services.js
+++ www/js/services.js
@@ -1,9 +1,6 @@
 // var adminurl = "http://104.198.28.29:80/api/"; //server
-<<<<<<< HEAD
 var adminurl = "http://192.168.2.24:1337/api/"; //server
-=======
-var adminurl = "http://192.168.2.24:1337/api/"; //Nargis Machine
->>>>>>> 51019b1dcfed29c8508574b067811d8da0609dff
+
 
 // var imgpath = adminurl + "uploadfile/getupload?file=";
 var imgurl = adminurl + "upload/";
