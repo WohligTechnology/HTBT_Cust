1309e3fd372b851a9415851c28251eef780139a2
Pranay Joshi <pranayj1449517@gmail.com>
2017-06-05 12:39:59 +0530

    directive

--- www/js/app.js
+++ www/js/app.js
@@ -415,4 +415,16 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ionic-datepicker', '
 
         return input;
     };
-});
+})
+
+ .directive("limitTo", [function() {
+      return {
+          restrict: "A",
+          link: function(scope, elem, attrs) {
+              var limit = parseInt(attrs.limitTo);
+              angular.element(elem).on("keypress", function(e) {
+                  if (this.value.length == limit) e.preventDefault();
+              });
+          }
+      }
+  }]);
